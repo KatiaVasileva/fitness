5e47def83c650783647da416d50c5558e075bf14
KatiaVasileva <eka.vasileva@gmail.com>
2024-10-16 17:16:25 +0500

    routing added

---
+++ fitness/src/components/Routing/Routing.tsx
@@ -0,0 +1,26 @@
+// import PrivateRoute from "../../routes/PrivateRoute";
+import { AppRoutes } from "../../lib/appRoutes";
+import MainPage from "../../pages/MainPage";
+import LoginPage from "../../pages/LoginPage";
+import RegisterPage from "../../pages/RegisterPage";
+import NotFoundPage from "../../pages/NotFoundPage";
+import { Route, Routes } from "react-router-dom";
+
+export default function Routing() {
+  return (
+    <>
+      <Routes>
+        {/* <Route element={<PrivateRoute />}>
+          <Route path={AppRoutes.MAIN} element={<MainPage />}>
+            <Route path={AppRoutes.EXIT} element={<ExitPage />} />
+          </Route>
+        </Route> */}
+
+        <Route path={AppRoutes.MAIN} element={<MainPage />} />
+        <Route path={AppRoutes.LOGIN} element={<LoginPage />} />
+        <Route path={AppRoutes.REGISTER} element={<RegisterPage />} />
+        <Route path={AppRoutes.NOT_FOUND} element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
---
+++ fitness/src/lib/appRoutes.ts
@@ -0,0 +1,7 @@
+export const AppRoutes = {
+    LOGIN: "/login",
+    REGISTER: "/register",
+    MAIN: "/",
+    // EXIT: "/exit",
+    NOT_FOUND: "*"
+  };
